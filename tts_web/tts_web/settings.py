d621b8395fe4e0848c430372c7f991244f5840ec
8ivlvi8 <letam2002pk@gmail.com>
2024-04-16 12:15:12 +0700

    fix homepage css

--- tts_web/tts_web/settings.py
+++ tts_web/tts_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qoij&w6f1p%$@bxt#r58=kd5^z0t80-)xp091it&f07#@=cebu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition

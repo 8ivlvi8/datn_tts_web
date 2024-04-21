563c69b08c7ea0b5c06d9b7713aed7685bff5ba0
8ivlvi8 <letam2002pk@gmail.com>
2024-04-21 20:47:02 +0700

    thêm api để tạo audio và api để tạo api audio với stream

--- tts_web/tts_api/urls.py
+++ tts_web/tts_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tts_api/', views.TTS_API_Get_Text.as_view()),
+    path('tts_api/gettext/', views.TTS_API_Get_Text.as_view()),
+    path('tts_api/getaudio/', views.TTS_API_Get_Audio.as_view()),
+    path('tts_api/getaudiostream/', views.TTS_API_Get_Audio_Stream.as_view()),
 ]
--- tts_web/tts_api/views.py
+++ tts_web/tts_api/views.py
@@ -1,17 +1,9 @@
-from tts_api.serializers import TextToSpeechSerializer
-from rest_framework.parsers import JSONParser
-from django.http import HttpResponse, JsonResponse
-from django.http import JsonResponse
+from django.http import JsonResponse, StreamingHttpResponse
 from rest_framework.views import APIView
 import requests
 from bs4 import BeautifulSoup
-
-# Chuyển đổi các tham số truy vấn thành một từ điển
-def convert_query_params_to_dict(query_params):
-    params = {}
-    for key, value in query_params.items():
-        params[key] = value
-    return params
+import edge_tts
+import asyncio
 
 def get_text_by_link_and_element(link, element):
     response = requests.get(link)
@@ -23,15 +15,61 @@ def get_text_by_link_and_element(link, element):
     return str(text)
 
 
-class TTS_API_Get_Text(APIView): 
-    def get(self, request, format=None):
+class TTS_API_Get_Text(APIView):
+    def post(self, request, format=None):
         url = request.query_params['url']
         element = request.query_params['element']
         # print("url: ", url)
         # print("element: ", element)
         try:
-            extracted_text = get_text_by_link_and_element(url, element)
-            return JsonResponse({'extracted_text': extracted_text}, status=200)
+            extracted_text_html = get_text_by_link_and_element(url, element)
+            extracted_text = extracted_text_html.replace("<br>", " ")
+            return JsonResponse({'extracted_text_html': extracted_text_html, 'extracted_text': extracted_text}, status=200)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
+
+class TTS_API_Get_Audio(APIView):
+    async def generate_audio(self, text, voice, output_file):
+        communicate = edge_tts.Communicate(text, voice)
+        await communicate.save(output_file)
+
+    def post(self, request, format=None):
+        # Nhận dữ liệu từ request
+        text = str(request.query_params['text'])
+        voice = str(request.query_params['voice'])
+        output_file = str(request.query_params['output_file'])
+        # voice = str(request.data.get('voice'))
+        # output_file = str(request.data.get('output_file'))
+
+        # Kiểm tra và chạy hàm tạo audio bất đồng bộ
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_until_complete(
+                self.generate_audio(text, voice, output_file))
+        finally:
+            loop.close()
+
+        return JsonResponse({'message': f'Audio file {output_file} has been generated.'})
+
+
+async def generate_audio_stream(text, voice, output_file):
+    communicate = edge_tts.Communicate(text, voice)
+    async for chunk in communicate.stream():
+        if chunk["type"] == "audio":
+            yield chunk["data"]
+        elif chunk["type"] == "WordBoundary":
+            print(f"WordBoundary: {chunk}")
+
+class TTS_API_Get_Audio_Stream(APIView):
+    def post(self, request, format=None):
+        text = request.query_params.get('text', '')
+        voice = request.query_params.get('voice', 'vi-VN-HoaiMyNeural')
+        output_file = request.query_params.get('output_file', 'tts_audio.mp3')
+
+        # Trả về dữ liệu âm thanh dưới dạng streaming
+        response = StreamingHttpResponse(generate_audio_stream(text, voice, output_file), content_type='audio/mpeg')
+        response['Content-Disposition'] = f'attachment; filename="{output_file}"'
+        return response
 

03dbf4b0d9e4e74bb47bf845c3a76d998ff176d7
8ivlvi8 <letam2002pk@gmail.com>
2024-04-23 23:32:22 +0700

    tạo model cho nguồn truyện

--- tts_web/web/admin.py
+++ tts_web/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import nguonTruyen
 
 # Register your models here.
+admin.site.register(nguonTruyen)
+class nguonTruyenAdmin(admin.ModelAdmin):
+    list_display = ("id","tenNguonTruyen","link","element")
--- tts_web/web/models.py
+++ tts_web/web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class nguonTruyen(models.Model):
+    id = models.AutoField(primary_key=True)
+    tenNguonTruyen = models.CharField(max_length=200)
+    link = models.URLField(max_length=200, blank=True)
+    element = models.CharField(max_length=200, blank=True)
+    nextElement = models.CharField(max_length=200, blank=True)
+    previousElement = models.CharField(max_length=200, blank=True)
+    

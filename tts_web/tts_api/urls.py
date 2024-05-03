fb7bd0278033af5937da57accd8ca87fa5c281ab
8ivlvi8 <letam2002pk@gmail.com>
2024-05-03 23:28:06 +0700

    thêm path tới api lấy element playaudio

--- tts_web/tts_api/urls.py
+++ tts_web/tts_api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('tts_api/getaudio/', views.TTS_API_Get_Audio.as_view()),
     path('tts_api/getaudiostream/', views.TTS_API_Get_Audio_Stream.as_view()),
     path('tts_api/getlistchapters/', views.TTS_API_Get_List_Chapter.as_view()),
+    path('tts_api/getaudioplayer/', views.TTS_API_Get_Audio_Player.as_view()),
 ]

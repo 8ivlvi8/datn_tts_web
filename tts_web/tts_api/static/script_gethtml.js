0650d8eec5d4f3ee1c47a0fdb0d5ab42679eb7da
8ivlvi8 <letam2002pk@gmail.com>
2024-05-05 09:00:18 +0700

    thêm phát audio, xử lý lưu tốc độ phát

--- tts_web/tts_api/static/script_gethtml.js
+++ tts_web/tts_api/static/script_gethtml.js
@@ -48,28 +48,193 @@ function direct(href) {
     console.log('Đã click vào link với href là: ' + href);
     if (href.startsWith("https://truyenfull.vn"))
         window.location.href = prefix + href;
-    else 
+    else
         window.location.href = href;
-}
-
-// Xử lý các link chuyển chương
-var content = document.getElementById('chapter-c');
-if (content) {
-    console.log("đang ở chap truyện");
-    var prev_chap_element = document.getElementById('prev_chap');
-    var next_chap_element = document.getElementById('next_chap');
-    prev_chap_element.setAttribute('href', prefix + prev_chap_element.getAttribute('href'));
-    next_chap_element.setAttribute('href', prefix + next_chap_element.getAttribute('href'));
-    $.get("/api/tts_api/getaudioplayer/", function (data) {
-        $(".chapter-start").replaceWith(data);
-    });
-
 }
 
 // Xóa element thứ 4 (web truyện tranh)
 var dropdownItems = document.querySelectorAll('.control.nav.navbar-nav > .dropdown');
 if (dropdownItems.length == 5)
     dropdownItems[3].remove();
+function updatecurrentSpeed() {
+    // Lưu trạng thái của checkbox vào localStorage
+    localStorage.setItem("currentSpeed", currentSpeedDisplay.textContent);
+}
 
 
-    
+var playAudioButton = null;
+var prev_chapterButton = null;
+var next_chapterButton = null;
+var currentSpeedDisplay = null;
+var audioPlayer = null;
+var autoPlay = null;
+var autoPlayValue = localStorage.getItem("autoPlay");
+var textData = [];
+var next_chap_element = null;
+var prev_chap_element = null;
+var currentSpeedValue = localStorage.getItem("currentSpeed");
+
+// Xử lý các link chuyển chương
+var content = document.getElementById('chapter-c');
+if (content) {
+    console.log("đang ở chap truyện");
+    prev_chap_element = document.getElementById('prev_chap');
+    next_chap_element = document.getElementById('next_chap');
+    prev_chap_element.setAttribute('href', prefix + prev_chap_element.getAttribute('href'));
+    next_chap_element.setAttribute('href', prefix + next_chap_element.getAttribute('href'));
+    $.get("/api/tts_api/getaudioplayer/", function (data) {
+        $(".chapter-start").replaceWith(data);
+        // Lấy đối tượng button Phát
+        playAudioButton = document.getElementById('playAudioButton');
+        // Lấy đối tượng tốc độ phát 
+        currentSpeedDisplay = document.getElementById("currentSpeed");
+        // Lấy đối tượng audioPlayer
+        audioPlayer = document.getElementById("audioPlayer");
+
+        // Lấy đối tượng tự động phát
+        autoPlay = document.getElementById("autoPlay");
+        // Nếu có dữ liệu trong localStorage, cập nhật checkbox
+        if (autoPlayValue !== null)
+            autoPlay.checked = (autoPlayValue === "true");
+        else
+            autoPlay.checked = false;
+        function updateAutoPlay() {
+            // Lưu trạng thái của checkbox vào localStorage
+            localStorage.setItem("autoPlay", autoPlay.checked);
+        }
+        // Lắng nghe sự kiện change thay đổi
+        autoPlay.addEventListener("change", updateAutoPlay);
+
+        // Lấy đối tượng tốc độ phát 
+        currentSpeedDisplay = document.getElementById("currentSpeed");
+        if (currentSpeedValue === null) {
+            audioPlayer.playbackRate = 1.6;
+            currentSpeedDisplay.textContent = audioPlayer.playbackRate.toFixed(1); // Hiển thị tốc độ phát hiện tại
+            localStorage.setItem("currentSpeed", "1.6");
+        } else{
+            currentSpeedDisplay.textContent = currentSpeedValue;
+            audioPlayer.playbackRate = currentSpeedValue;
+        }
+        // Lắng nghe sự kiện change thay đổi
+        currentSpeedDisplay.addEventListener("change", updatecurrentSpeed);
+
+        // Đặt tốc độ phát mặc định của audioPlayer khi trang được tải
+
+        // Lấy nội dung của phần tử có id là "chapter-c"
+        let paragraphs = content.textContent.split('\n');
+        // Loại bỏ các khoảng trắng dư thừa ở đầu và cuối mỗi đoạn văn bản và loại bỏ các đoạn văn bản rỗng
+        textData = paragraphs.map(function (paragraph) {
+            return paragraph.trim();
+        }).filter(function (paragraph) {
+            return paragraph !== '';
+        });
+
+        let title = document.querySelector('.chapter-title').getAttribute('title');
+        console.log(title);
+        textData.unshift(title);
+
+        // // Hiển thị mảng các đoạn văn bản đã được làm sạch trong console
+        // console.log(textData);
+
+        if (autoPlayValue === "true")
+            playAudio();
+    });
+}
+
+
+// Function để giảm tốc độ phát
+function decreaseSpeed() {
+    if (audioPlayer.playbackRate > 1) {
+        audioPlayer.playbackRate -= 0.1;
+        currentSpeedDisplay.textContent = audioPlayer.playbackRate.toFixed(1);
+        updatecurrentSpeed();
+    }
+}
+// Function để tăng tốc độ phát
+function increaseSpeed() {
+    if (audioPlayer.playbackRate < 2.5) {
+        audioPlayer.playbackRate += 0.1;
+        currentSpeedDisplay.textContent = audioPlayer.playbackRate.toFixed(1);
+        updatecurrentSpeed();
+    }
+}
+// Hàm để hiển thị biểu tượng loading
+function showLoader() {
+    playAudioButton.style.display = 'none';
+    document.getElementById('loader').style.display = 'block';
+}
+// Hàm để ẩn biểu tượng loading
+function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+    playAudioButton.style.display = 'block';
+}
+
+
+let currentIndex = 0; // Chỉ số để theo dõi văn bản hiện đang được phát
+
+// Hàm để phát audio từ API
+function playAudio() {
+    if (currentIndex >= textData.length) {
+        console.log('Phát audio hoàn thành');
+        return;
+    }
+    showLoader();
+    const url = 'http://letam.myftp.org:8686/api/tts_api/getaudiostream/';
+    // Mảng để lưu trữ tất cả các Promise fetch audio
+    audioPromises = [];
+    // Duyệt qua các đoạn văn bản từ currentIndex đến cuối văn bản
+    for (let i = currentIndex; i < textData.length; i++) {
+        // Thêm Promise của mỗi đoạn văn bản vào mảng audioPromises
+        audioPromises.push(fetchAudio(url, textData[i]));
+    }
+    // Khởi tạo một hàm async để xử lý việc fetch và phát audio
+    async function playAudioSequentially(audioPromises) {
+        for (let i = 0; i < audioPromises.length; i++) {
+            audioURL = await audioPromises[i]; // Chờ cho đến khi Promise hoàn thành
+            // Phát audio của đoạn hiện tại
+            audioPlayer.src = audioURL;
+            audioPlayer.playbackRate = currentSpeedDisplay.textContent;
+            audioPlayer.play();
+            // Chờ cho đến khi audio hiện tại kết thúc trước khi chuyển sang đoạn tiếp theo
+            await new Promise(resolve => audioPlayer.onended = resolve);
+        }
+    }
+    // Gọi hàm vừa tạo với danh sách Promise
+    playAudioSequentially(audioPromises).catch(error => {
+        console.error('Error playing audio:', error);
+        hideLoader(); // Ẩn biểu tượng loading trong trường hợp lỗi
+    }).finally(function () {
+        hideLoader();
+        if (autoPlay.checked)
+            next_chap_element.click();
+    });
+}
+// Hàm để fetch audio từ API
+function fetchAudio(url, text) {
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                text: text,
+                voice: 'vi-VN-HoaiMyNeural'
+            })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch audio from API');
+                }
+                return response.blob(); // Chuyển đổi response thành Blob
+            })
+            .then(blob => {
+                const audioURL = URL.createObjectURL(blob); // Tạo URL từ Blob
+                resolve(audioURL);
+            })
+            .catch(error => {
+                console.error('Error fetching audio:', error);
+                reject(error);
+            });
+    });
+}

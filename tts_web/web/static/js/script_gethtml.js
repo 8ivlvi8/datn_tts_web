9533ea08503c317718d532cd2f9870f117638a61
8ivlvi8 <letam2002pk@gmail.com>
2024-05-07 22:50:19 +0700

    sửa button chuyển chương, thêm comment

--- tts_web/web/static/js/script_gethtml.js
+++ tts_web/web/static/js/script_gethtml.js
@@ -1,18 +1,18 @@
+// Xóa các element quảng cáo
 function removeAds() {
-    // Xóa các element quảng cáo
     for (let index = 0; index < 8; index++)
         setTimeout(() => {
             var adsElements = document.getElementsByClassName('ads-iads');
             for (let item of adsElements)
                 item.remove();
-            var adshome = ['hot-select', 'banner_image_home', 'is_show_slide', 'ads-300x250-detail-truyen-top', 'shoppe_ads_fly', 'ads-chapter-bottom-lien-quan']
+            var adshome = ['balloon-ads', 'hot-select', 'banner_image_home', 'is_show_slide', 'ads-300x250-detail-truyen-top', 'shoppe_ads_fly', 'ads-chapter-bottom-lien-quan']
             for (let item of adshome)
                 try {
                     document.getElementById(item).remove();
                 } catch (error) {
                 }
             console.log(index);
-        }, index * 1500);
+        }, index * 1500); // Việc xóa lặp lại do các element load chậm
 }
 removeAds();
 
@@ -21,12 +21,12 @@ var formElement = document.querySelector('.navbar-form.navbar-right');
 formElement.action = "";
 
 
-// Xóa Ngày Tháng Năm
+// Xóa Ngày Tháng Năm ở mục top truyện
 row_top_nav = document.querySelector('.row.top-nav');
 if (row_top_nav)
     row_top_nav.remove();
 
-// Xử lý chọn chương
+// Xử lý khi bấm chọn chương
 list_chapter = document.querySelector('.btn.btn-success.btn-chapter-nav.chapter_jump');
 if (list_chapter) {
     list_chapter.classList.remove('chapter_jump');
@@ -34,6 +34,7 @@ if (list_chapter) {
     list_chapter.setAttribute('onclick', 'get_list_chapter()');
 }
 
+// Hàm lấy danh sách truyện và xử lý khi chọn chương
 function get_list_chapter() {
     var truyen_id = $("#truyen-id").val();
     $.get("/api/tts_api/getlistchapters/", { truyen_id: truyen_id }, function (data) {
@@ -58,7 +59,7 @@ function get_list_chapter() {
     });
 }
 
-// Xử lý khi click vào các thẻ a
+// Xử lý các thẻ a khi click vào
 var prefix = "/nghetruyen/?url=";
 var anchorLinks = document.querySelectorAll('a');
 anchorLinks.forEach(function (anchor) {
@@ -75,7 +76,7 @@ function direct(href) {
         window.location.href = href;
 }
 
-// Xóa element thứ 4 (web truyện tranh)
+// Xóa element thứ 4 (web truyện tranh) và thay bằng trang Text to speech
 var dropdownItems = document.querySelectorAll('.control.nav.navbar-nav > .dropdown');
 if (dropdownItems.length == 5) {
     const dropdownItem = dropdownItems[3];
@@ -84,9 +85,13 @@ if (dropdownItems.length == 5) {
     dropdownItem.querySelector('a').innerHTML = '<span class="glyphicon glyphicon-book" style="margin-right: 3px;"></span>Text to speech'
 }
 
+// Hàm cập nhật tốc độ phát audio vào localStorage
 function updatecurrentSpeed() {
-    // Lưu trạng thái của checkbox vào localStorage
     localStorage.setItem("currentSpeed", currentSpeedDisplay.textContent);
+}
+// Hàm cập nhật tự động phát audio vào localStorage
+function updateAutoPlay() {
+    localStorage.setItem("autoPlay", autoPlay.checked);
 }
 
 // Các biến dùng khi trong chương truyện
@@ -98,18 +103,23 @@ var audioPlayer = null;
 var autoPlay = null;
 var autoPlayValue = localStorage.getItem("autoPlay");
 var textData = [];
-var next_chap_element = null;
-var prev_chap_element = null;
+var next_chap_elements = null;
+var prev_chap_elements = null;
 var currentSpeedValue = localStorage.getItem("currentSpeed");
 
-// Xử lý các link chuyển chương
+// Xử lý khi ở trong chapter
 var content = document.getElementById('chapter-c');
 if (content) {
-    console.log("đang ở chap truyện");
-    prev_chap_element = document.getElementById('prev_chap');
-    next_chap_element = document.getElementById('next_chap');
-    prev_chap_element.setAttribute('href', prefix + prev_chap_element.getAttribute('href'));
-    next_chap_element.setAttribute('href', prefix + next_chap_element.getAttribute('href'));
+    // Cập nhật link cho các thẻ chuyển chương
+    prev_chap_elements = document.querySelectorAll("#prev_chap");
+    next_chap_elements = document.querySelectorAll("#next_chap");
+    prev_chap_elements.forEach(function (prev_chap_element) {
+        prev_chap_element.setAttribute('href', prefix + prev_chap_element.getAttribute('href'));
+    })
+    next_chap_elements.forEach(function (next_chap_element) {
+        next_chap_element.setAttribute('href', prefix + next_chap_element.getAttribute('href'));
+    })
+    // Thay đổi phần tử chapter-start có sẵn thành menu phát audio
     $.get("/api/tts_api/getaudioplayer/", function (data) {
         $(".chapter-start").replaceWith(data);
         // Lấy đối tượng button Phát
@@ -119,36 +129,27 @@ if (content) {
         // Lấy đối tượng audioPlayer
         audioPlayer = document.getElementById("audioPlayer");
 
-        // Lấy đối tượng tự động phát
+        // Lấy giá trị tự động phát để lưu và cập nhật
         autoPlay = document.getElementById("autoPlay");
-        // Nếu có dữ liệu trong localStorage, cập nhật checkbox
         if (autoPlayValue !== null)
             autoPlay.checked = (autoPlayValue === "true");
         else
             autoPlay.checked = false;
-        function updateAutoPlay() {
-            // Lưu trạng thái của checkbox vào localStorage
-            localStorage.setItem("autoPlay", autoPlay.checked);
-        }
-        // Lắng nghe sự kiện change thay đổi
         autoPlay.addEventListener("change", updateAutoPlay);
 
-        // Lấy đối tượng tốc độ phát 
+        // Lấy giá trị tốc độ phát để lưu và cập nhật 
         currentSpeedDisplay = document.getElementById("currentSpeed");
         if (currentSpeedValue === null) {
             audioPlayer.playbackRate = 1.6;
-            currentSpeedDisplay.textContent = audioPlayer.playbackRate.toFixed(1); // Hiển thị tốc độ phát hiện tại
+            currentSpeedDisplay.textContent = audioPlayer.playbackRate.toFixed(1);
             localStorage.setItem("currentSpeed", "1.6");
         } else {
             currentSpeedDisplay.textContent = currentSpeedValue;
             audioPlayer.playbackRate = currentSpeedValue;
         }
-        // Lắng nghe sự kiện change thay đổi
         currentSpeedDisplay.addEventListener("change", updatecurrentSpeed);
 
-        // Đặt tốc độ phát mặc định của audioPlayer khi trang được tải
-
-        // Lấy nội dung của phần tử có id là "chapter-c"
+        // Lấy nội dung của chapter
         let paragraphs = content.textContent.split('\n');
 
         // Loại bỏ các khoảng trắng dư thừa ở đầu và cuối mỗi đoạn văn bản và loại bỏ các đoạn văn bản rỗng
@@ -158,45 +159,36 @@ if (content) {
             return paragraph !== '';
         });
 
-        // Thêm tiêu đề chương vào đầu mảng
-        let title = document.querySelector('.chapter-title').getAttribute('title');
-        textData.unshift(title);
-
-        // Tạo một mảng mới để chứa các đoạn văn với số lượng câu tăng dần
+        // Tạo một mảng mới để chứa các đoạn văn với độ dài tăng dần
         let processedTextData = [];
         let currentParagraph = '';
-        let length = 0; // Số lượng câu ban đầu là 1
-        // Lặp qua từng câu trong textData
+        let length = 0;  
         textData.forEach(function (sentence) {
-            // Thêm câu vào đoạn hiện tại
             currentParagraph += sentence + '\n';
-
-            // Nếu số câu trong đoạn hiện tại đạt đến số lượng câu mong muốn, thêm đoạn vào mảng và bắt đầu đoạn mới
             if (currentParagraph.length > length) {
-                processedTextData.push(currentParagraph.trim());
+                processedTextData.push(currentParagraph);
                 currentParagraph = '';
-                length += 70;
+                length += 70; // Mỗi lần tăng độ dài tối đa thêm 70 ký tự
             }
         });
-
         // Nếu còn đoạn cuối cùng chưa được thêm vào mảng processedTextData, thêm nó vào
-        if (currentParagraph !== '') {
-            processedTextData.push(currentParagraph.trim());
-        }
-        console.log(processedTextData);
+        if (currentParagraph !== '')
+            processedTextData.push(currentParagraph);
 
         textData = processedTextData;
+        // Thêm tiêu đề chương vào phần tử đầu mảng
+        let title = document.querySelector('.chapter-title').getAttribute('title');
+        textData[0] = title + textData[0]
+        console.log(textData);
 
-        // // Hiển thị mảng các đoạn văn bản đã được làm sạch trong console
-        // console.log(textData);
-
+        // Nếu bật tự động phát, bấm vào nút Phát
         if (autoPlayValue === "true")
             playAudioButton.click();
     });
 }
 
 
-// Function để giảm tốc độ phát
+// Hàm giảm tốc độ phát
 function decreaseSpeed() {
     if (audioPlayer.playbackRate > 1) {
         audioPlayer.playbackRate -= 0.1;
@@ -204,7 +196,7 @@ function decreaseSpeed() {
         updatecurrentSpeed();
     }
 }
-// Function để tăng tốc độ phát
+// Hàm tăng tốc độ phát
 function increaseSpeed() {
     if (audioPlayer.playbackRate < 2.5) {
         audioPlayer.playbackRate += 0.1;
@@ -260,7 +252,7 @@ function playAudio() {
     }).finally(function () {
         hideLoader();
         if (autoPlay.checked)
-            next_chap_element.click();
+            next_chap_element[0].click();
     });
 }
 // Hàm để fetch audio từ API
